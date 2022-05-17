8c8364ca54569a957431781af7ae25a0be783403
AlexanderLitvyakov <alexander39.79@gmail.com>
2022-05-17 14:32:43 +0200

    Изменение стилей резюме

--- script.js
+++ script.js
@@ -1,66 +1,196 @@
-var canvas = document.querySelector('canvas');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    var ut, st = Date.now();
+var canvas = document.querySelector('.line');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var ut, st = Date.now();
 
-    function initShaders (gl, vertexShaderId, fragmentShaderId) {
-        var vertexEl = document.querySelector(vertexShaderId);
-        var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-        gl.shaderSource(vertexShader, vertexEl.text);
-        gl.compileShader(vertexShader);
+function initShaders(gl, vertexShaderId, fragmentShaderId) {
+    var vertexEl = document.querySelector(vertexShaderId);
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexEl.text);
+    gl.compileShader(vertexShader);
 
-        if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS) ) {
-            debugger
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+        debugger
+    }
+
+    var fragmentEl = document.querySelector(fragmentShaderId);
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentEl.text);
+    gl.compileShader(fragmentShader);
+
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+        debugger
+    }
+
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    gl.useProgram(program);
+
+    return program;
+}
+
+function initGraphics() {
+    gl = canvas.getContext('webgl');
+    var width = canvas.width;
+    var height = canvas.height;
+    gl.viewport(0, 0, width, height);
+
+    var program = initShaders(gl, '#sv', '#sf');
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([-1, 1, -1, -1, 1, -1, 1, 1]),
+        gl.STATIC_DRAW
+    );
+
+    var vPosition = gl.getAttribLocation(program, 'vPosition');
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    ut = gl.getUniformLocation(program, 'time');
+    var resolution = new Float32Array([canvas.width, canvas.height]);
+    gl.uniform2fv(gl.getUniformLocation(program, 'resolution'), resolution);
+}
+
+function render() {
+    gl.uniform1f(ut, (Date.now() - st) / 1000);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+    requestAnimationFrame(render);
+}
+
+initGraphics();
+render();
+
+// -----Firework----
+window.addEventListener("resize", resizeCanvas, false);
+window.addEventListener("DOMContentLoaded", onLoad, false);
+
+window.requestAnimationFrame =
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+        window.setTimeout(callback, 1000 / 60);
+    };
+
+var canvas, ctx, w, h, particles = [], probability = 0.04,
+    xPoint, yPoint;
+
+function onLoad() {
+    canvas = document.getElementById("fireworks");
+    ctx = canvas.getContext("2d");
+    resizeCanvas();
+
+    window.requestAnimationFrame(updateWorld);
+}
+
+function resizeCanvas() {
+    if (!!canvas) {
+        w = canvas.width = window.innerWidth;
+        h = canvas.height = window.innerHeight;
+    }
+}
+
+function updateWorld() {
+    update();
+    paint();
+    window.requestAnimationFrame(updateWorld);
+}
+
+function update() {
+    if (particles.length < 500 && Math.random() < probability) {
+        createFirework();
+    }
+    var alive = [];
+    for (var i = 0; i < particles.length; i++) {
+        if (particles[i].move()) {
+            alive.push(particles[i]);
         }
+    }
+    particles = alive;
+}
 
-        var fragmentEl = document.querySelector(fragmentShaderId);
-        var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-        gl.shaderSource(fragmentShader, fragmentEl.text);
-        gl.compileShader(fragmentShader);
+function paint() {
+    ctx.globalCompositeOperation = 'source-over';
+    ctx.fillStyle = "rgba(0,0,0,0.2)";
+    ctx.fillRect(0, 0, w, h);
+    ctx.globalCompositeOperation = 'lighter';
+    for (var i = 0; i < particles.length; i++) {
+        particles[i].draw(ctx);
+    }
+}
 
-        if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-            debugger
+function createFirework() {
+    xPoint = Math.random() * (w - 200) + 100;
+    yPoint = Math.random() * (h - 200) + 100;
+    var nFire = Math.random() * 50 + 100;
+    var c = "rgb(" + (~~(Math.random() * 200 + 55)) + ","
+        + (~~(Math.random() * 200 + 55)) + "," + (~~(Math.random() * 200 + 55)) + ")";
+    for (var i = 0; i < nFire; i++) {
+        var particle = new Particle();
+        particle.color = c;
+        var vy = Math.sqrt(25 - particle.vx * particle.vx);
+        if (Math.abs(particle.vy) > vy) {
+            particle.vy = particle.vy > 0 ? vy : -vy;
         }
-
-        var program = gl.createProgram();
-        gl.attachShader(program, vertexShader);
-        gl.attachShader(program, fragmentShader);
-        gl.linkProgram(program);
-        gl.useProgram(program);
-
-        return program;
+        particles.push(particle);
     }
+}
 
-    function initGraphics () {
-        gl = canvas.getContext('webgl');
-        var width = canvas.width;
-        var height = canvas.height;
-        gl.viewport(0, 0, width, height);
+function Particle() {
+    this.w = this.h = Math.random() * 4 + 1;
 
-        var program = initShaders(gl, '#sv', '#sf');
-        var buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    this.x = xPoint - this.w / 2;
+    this.y = yPoint - this.h / 2;
 
-        gl.bufferData(
-                gl.ARRAY_BUFFER,
-                new Float32Array([-1, 1, -1, -1, 1, -1, 1, 1]),
-                gl.STATIC_DRAW
-        );
+    this.vx = (Math.random() - 0.5) * 10;
+    this.vy = (Math.random() - 0.5) * 10;
 
-        var vPosition = gl.getAttribLocation(program, 'vPosition');
-        gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(vPosition);
+    this.alpha = Math.random() * .5 + .5;
 
-        ut = gl.getUniformLocation(program, 'time');
-        var resolution = new Float32Array([canvas.width, canvas.height]);
-        gl.uniform2fv(gl.getUniformLocation(program, 'resolution'), resolution);
+    this.color;
+}
+
+Particle.prototype = {
+    gravity: 0.05,
+    move: function () {
+        this.x += this.vx;
+        this.vy += this.gravity;
+        this.y += this.vy;
+        this.alpha -= 0.01;
+        if (this.x <= -this.w || this.x >= screen.width ||
+            this.y >= screen.height ||
+            this.alpha <= 0) {
+            return false;
+        }
+        return true;
+    },
+    draw: function (c) {
+        c.save();
+        c.beginPath();
+
+        c.translate(this.x + this.w / 2, this.y + this.h / 2);
+        c.arc(0, 0, this.w, 0, Math.PI * 2);
+        c.fillStyle = this.color;
+        c.globalAlpha = this.alpha;
+
+        c.closePath();
+        c.fill();
+        c.restore();
     }
+}
 
-    function render () {
-        gl.uniform1f(ut, (Date.now() - st) / 1000);
-        gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
-        requestAnimationFrame(render);
-    }
+// обработчик кнопки-----------
 
-    initGraphics();
-    render();
+button.onclick = function () {
+    fireworks.style.cssText = "display: block;";
+};
+fireworks.onclick = function () {
+    fireworks.style.cssText = "display: none;";
+}
